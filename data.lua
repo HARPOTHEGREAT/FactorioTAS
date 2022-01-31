ffa4dee0c2bcb67569dfa419eeec5ce71d5a340c
TFJamin <brh12@mindspring.com>
2022-01-31 04:23:04 -0500

    Update data.lua

--- data.lua
+++ data.lua
@@ -4,18 +4,21 @@ data:extend({
       type = 'custom-input',
       name = 'tas-tools:pause-unpause',
       consuming = nil,
+      key_sequence = '',
       },
     
     {
       type = 'custom-input',
       name = 'tas-tools:frame-advance',
       consuming = nil,
+      key_sequence = '',
       },
     
     {
       type = 'custom-input',
       name = 'tas-tools:toggle-input-display',
       consuming = nil,
+      key_sequence = '',
       },
     
     })
