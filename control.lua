e6a669b9b8a38bdd4ae966115b3ab901ea755f3e
HARPOTHEGREAT <c.hayes.harrison@gmail.com>
2022-02-02 12:58:45 -0500

    added "verbose logging" for gamespeed gui elements

--- control.lua
+++ control.lua
@@ -28,7 +28,7 @@ script.on_event(defines.events.on_player_created, function(event)
     controls_flow_pause.tas_tickadv.enabled = false --make advance tick start disabled
         
     controls_flow_speed.add{type="slider", name="tas_gamespeed_slider", value=1, minimum_value=0.01, maximum_value=2, value_step=0.01, discrete_values=false, style="notched_slider"} --add gamespeed slider
-    controls_flow_speed.add{type="textfield", name="tas_gamespeed_textfield", text="1", numeric=true, allow_decimal=true, allow_negative=false, style="tas_controls_textfield"} --add gamespeed textfield
+    controls_flow_speed.add{type="textfield", name="tas_gamespeed_textfield", text="1", numeric=true, allow_decimal=true, allow_negative=false, style="alider_value_textfield"} --add gamespeed textfield
 end)
 
 --pause game if unpaused, unpause game if paused
@@ -93,8 +93,6 @@ local function advance_frame()
     
     --game.ticks_to_run = 1
     
-    
-    
     end
 
 --listen for all gui clicks
@@ -102,7 +100,7 @@ script.on_event(defines.events.on_gui_click, function(event)
     if event.element.name == "tas_pause_toggle" then --check if the gui click was for the pause button
         pause_toggle()
     end
-        if event.element.name == "tas_tickadv" then --check if the gui click was for the tick advance button
+    if event.element.name == "tas_tickadv" then --check if the gui click was for the tick advance button
         advance_frame()
     end
 end)
@@ -110,7 +108,7 @@ end)
 --listen for all gui value changes
 script.on_event(defines.events.on_gui_value_changed, function(event)
     if event.element.name == "tas_controls_slider" then --check if the value change was for the gamespeed slider
-
+        game.print("slider changed")
         local new_speed_value = event.element.slider_value --get updated slider value
 
         local controls_flow = player.gui.screen.tas_main_frame.content_frame.controls_flow_speed
@@ -121,6 +119,7 @@ end)
 --listen for all gui text edits
 script.on_event(defines.events.on_gui_text_changed, function(event)
     if event.element.name == "tas_controls_textfield" then --check if the text edit was for the gamespeed textfield
+        game.print("textfield changed")
 
         local new_speed_count = tonumber(event.element.text) or 1 --get updated text and convert to number, set to 1 if blank
         local capped_speed_count --initialize slider count
@@ -131,14 +130,18 @@ script.on_event(defines.events.on_gui_text_changed, function(event)
         -- ensure slider is only set to values within it's range
         if new_speed_count > slider_max then --if more than slider maximum, set to maximum
             capped_speed_count = slider_max
+            game.print("new value too high")
         elseif new_speed_count < slider_min then --if less than slider minimum, set to slider minimum
             capped_speed_count = slider_min
+            game.print("new value too low")
         else
             capped_speed_count = new_speed_count --else, use actual value
+            game.print("new value accepted")
         end
 
         
         tas_gamespeed_slider.slider_value = capped_speed_count --set slider to new, capped value
+        game.print("slider updated")
     end
 end)
 
